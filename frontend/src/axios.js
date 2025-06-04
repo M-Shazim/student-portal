58131ba149779b7beceabd7b93701580c33e39e5
M-Shazim <s.n.bhatti45@gmail.com>
2025-06-04 14:18:13 +0500

    Fixed .api and .axios errors

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // backend API
+  baseURL: '/api', // Let nginx proxy /api to backend
 });
 
 export default api;
